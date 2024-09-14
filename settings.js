4183cd390f6ea32bd0b334f4f35fb2a39e17a912
RotexC <155334810+RotexC@users.noreply.github.com>
2024-09-14 10:35:51 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['','!','.',',','🐤','🗿']
 
-global.owner = ['639470983577']
+global.owner = ['2349114224240']
 global.botname = '𝐃𝐫𝐚𝐠𝐨𝐧𝐬 𝐃𝐨𝐱𝐳 𝐕𝟐 💀'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "'𝐃𝐫𝐚𝐠𝐨𝐧𝐬 𝐃𝐨𝐱𝐳 𝐕𝟐 💀"
